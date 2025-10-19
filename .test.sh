cc534ae9da91ab2173e6cbddbb812c6ba2ee0b0b
Hamed Akhavan <hakh.27@gmail.com>
2025-10-19 11:47:55 +0330

    fix: check if pdm is installed

--- .test.sh
+++ .test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Check if pdm is installed, if not install it
+command -v pdm >/dev/null 2>&1 || pip install pdm
 pdm install
 python -m mypy .
 python -m pytest -vv
